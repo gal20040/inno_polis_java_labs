761c4eff88c58dd592eb5ec34803059fbc1e746f
Artem Grodetskiy <gal20040@gmail.com>
2016-08-24 16:13:39 +0300

    added initial class for DoubleMess

---
+++ src/codetest/edu/innopolis/ru/DoubleMess/Solution.java
@@ -0,0 +1,18 @@
+package codetest.edu.innopolis.ru.DoubleMess;
+
+/**Statement
+ * You found the data in a very strange format. Then you understood, that previous developer mixed sign S, significand M and exponent E in a double! Now they are in the following order: MES. Fix it. Read input.txt file. Write fixed result to output.txt.
+
+ * Example:
+
+ input.txt
+ 1.0207E-320
+
+ output.txt
+ 1024.0
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
