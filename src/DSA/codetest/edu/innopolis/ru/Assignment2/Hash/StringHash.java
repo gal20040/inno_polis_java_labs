942322992f7f720080375e2eaf52693471c7d789
Artem Grodetskiy <gal20040@gmail.com>
2016-09-06 19:29:12 +0300

    добавил начало для Assignment-Hash

---
+++ src/DSA/codetest/edu/innopolis/ru/Assignment2/Hash/StringHash.java
@@ -0,0 +1,24 @@
+package DSA.codetest.edu.innopolis.ru.Assignment2.Hash;
+
+//https://www.google.ru/search?q=Linear+probing&oq=Linear+probing&aqs=chrome..69i57&sourceid=chrome&ie=UTF-8#newwindow=1&safe=strict&q=Linear+probing+Open+addressing
+//http://www.algolist.net/Data_structures/Hash_table/Open_addressing
+//https://en.wikipedia.org/wiki/Linear_probing
+//https://en.wikipedia.org/wiki/Open_addressing
+
+public class StringHash {
+    public static void main(String[] args) {
+        // Java uses polynomial function with base 31
+// for string hashing
+        String a = "A Test";
+        String b = "B Test";
+        String c = "C Test";
+        long power4 = 31 * 31 * 31 * 31;
+        System.out.println(a.hashCode() % power4);
+        System.out.println(b.hashCode() % power4);
+        System.out.println(c.hashCode() % power4);
+        a = "Question.";
+        b = "Question!";
+        System.out.println(a.hashCode() / 31);
+        System.out.println(b.hashCode() / 31);
+    }
+}
